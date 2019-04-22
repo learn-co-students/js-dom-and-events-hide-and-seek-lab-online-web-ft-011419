d0f13ecd5b141db2c00c81c8c98457866b509a1b
mikeygbd <mikey.soares@gmail.com>
2019-04-22 09:17:20 -0600

    Done.

--- index.js
+++ index.js
@@ -7,9 +7,19 @@ function nestedTarget() {
   return element
 }
 function increaseRankBy(n) {
-  const elements = document.querySelectorAll('.ranked-list li')
-    for(let i = 0; i < elements.lenght; i++) {
-       elemens[i].innerHTML = parseInt(elements[i].innerHTML) + n
-    }
+  let rankedListLi = document.querySelectorAll(".ranked-list li");
+
+  for (let i = 0; i < rankedListLi.length; i++) {
+    rankedListLi[i].innerHTML = parseInt(rankedListLi[i].innerHTML) + n;
+  }
+}
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let child = node.children[0]
+  while(child) {
+    node = child
+    child = node.children[0]
+  }
+  return node
 
 }
