d73b11082f6935f6df7b985820b9fed2778a2099
mikeygbd <mikey.soares@gmail.com>
2019-04-22 08:41:02 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector) {
+  const element = document.querySelector(selector)
+  return element
+}
+function nestedTarget() {
+  const element = document.querySelector(#nested .target)
+  return element
+}
+function increaseRankBy(n) {
+  const elements = document.querySelectorAll('.ranked-list')
+    for(let i = 0; i < elements.lenght; i++) {
+
+    }
+  
+}
