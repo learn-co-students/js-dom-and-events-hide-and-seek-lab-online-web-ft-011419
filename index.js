83dbde599853876a912c55558865a0319afb6e95
grangerl330 <grangerl330@gmail.com>
2019-04-24 09:53:26 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+};
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+};
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
+  };
+};
+
+function deepestChild() {
+  let grandNode = document.querySelector('#grand-node');
+
+  while (grandNode.children[0]) {
+    grandNode = grandNode.children[0];
+  };
+
+  return grandNode;
+};
